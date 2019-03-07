f8880cee16c8f353cb2a03519603bb6b170aa89c
brunomartins <brunocmartins@live.com.pt>
2019-03-07 17:49:31 +0000

    fix permission and add error

--- phase1/healthcare.pl
+++ phase1/healthcare.pl
@@ -1,22 +1,24 @@
 % Definições iniciais
-:- op(900, xfy, '::').
-
+:- dynamic utente/4.
 % -------------------------------------------------------
 % --------------- Predicados auxiliares -----------------
 % -------------------------------------------------------
 
 % Extensao do predicado nao: Q -> {V,F}
 nao(Q) :- Q, !, fail.
-nao(Q).
+nao(_).
 
 % Extensao do predicado solucoes: F, Q, S -> {V,F}
-solucoes(F,Q,S) :- Q, assert(tmp(F)), fail.
-solucoes(F,Q,S) :- construir(S, []).
+solucoes(F,Q,_) :- Q, assert(tmp(F)), fail.
+solucoes(_,_,S) :- construir(S, []).
 
 % Extensao do predicado construir: S1,S2 -> {V,F}
 construir(S1, S2) :- retract(tmp(X)), !, construir(S1, [X|S2]).
 construir(S,S).
 
+%--------------------------------------------------------
+%----------------- Predicados ---------------------------
+%--------------------------------------------------------
 utente( 0, 'Jose',     55, 'Rua dos Zecas').
 utente( 1, 'Joao',     21, 'Rua de Baixo').
 utente( 2, 'Manuel',   36, 'Rua Maria Albertina').
@@ -25,8 +27,7 @@ utente( 4, 'Maria',    73, 'Avenida Camoes').
 
 % Registo de utentes
 
-add_utente(Id, Nome, Idade, Cidade) :- \+ utente(Id, Nome, Idade, Cidade), Idade >= 0, Id >= 0,
-                                        assertz(utente(Id, Nome, Idade, Cidade)).
+add_utente(Id, Nome, Idade, Cidade) :-  nao(utente(Id, _, _ ,_)), Idade >= 0, Id >= 0,
+                                        assert(utente(Id, Nome, Idade, Cidade)).
 
-remove_utente(Id, _, _, _) :- retract(utente(Id, _, _, _)).
-
+remove_utente(Id) :- retract(utente(Id, _, _, _)).
