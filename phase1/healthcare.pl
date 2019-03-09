c9713471fc2547412dcbdbed18ca170e1b28cc94
Eduardo Barbosa <herulume@protonmail.com>
2019-03-09 10:50:52 +0000

    Add more utentes

--- phase1/healthcare.pl
+++ phase1/healthcare.pl
@@ -1,5 +1,6 @@
 % Definições iniciais
 :- dynamic utente/4.
+
 % -------------------------------------------------------
 % --------------- Predicados auxiliares -----------------
 % -------------------------------------------------------
@@ -16,21 +17,29 @@ solucoes(_,_,S) :- construir(S, []).
 construir(S1, S2) :- retract(tmp(X)), !, construir(S1, [X|S2]).
 construir(S,S).
 
+
 %--------------------------------------------------------
 %----------------- Predicados ---------------------------
 %--------------------------------------------------------
-utente( 0, 'Jose',     55, 'Rua dos Zecas').
-utente( 1, 'Joao',     21, 'Rua de Baixo').
-utente( 2, 'Manuel',   36, 'Rua Maria Albertina').
-utente( 3, 'Carlos',   43, 'Rua da Fabrica').
-utente( 4, 'Maria',    73, 'Avenida Camoes').
+%
+% Extensao do predicado utente: IdUt, Nome, Idade, Cidade -> {V,F}
+utente(0,     'Jose', 55,     'Porto').
+utente(1,     'Joao', 21,     'Braga').
+utente(2,   'Manuel', 36,     'Porto').
+utente(3,   'Carlos', 43, 'Guimaraes').
+utente(4,    'Maria', 73, 'Guimaraes').
+utente(5,    'Joana',  8,     'Porto').
+utente(6, 'Fernando', 49,    'Aveiro').
+utente(7,     'Joao', 29,    'Aveiro').
+utente(8,      'Ana', 40,     'Braga').
+utente(9, 'Catarina', 17,     'Braga').
 
-% Registo de utentes
-
+% Extensao do predicado add_utente: IdUt, Nome, Idade, Cidade -> {V,F}
 add_utente(Id, Nome, Idade, Cidade) :-
                                      nao(utente(Id, _, _ ,_)),
                                      Idade >= 0,
                                      Id >= 0,
                                      assert(utente(Id, Nome, Idade, Cidade)).
 
+% Extensao do predicado remove_utente: IdUt -> {V,F}
 remove_utente(Id) :- retract(utente(Id, _, _, _)).
