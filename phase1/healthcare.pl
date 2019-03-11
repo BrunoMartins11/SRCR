1dcd96c8f747d28d54273837a219ac2de0b3d1b6
Eduardo Barbosa <herulume@protonmail.com>
2019-03-11 10:29:55 +0000

    Part1 of q5

--- phase1/healthcare.pl
+++ phase1/healthcare.pl
@@ -73,3 +73,21 @@ instituicoes_servico(Servico, R) :-
 
 % Extensao do predicado instituicoes_id: Id, R -> {V,F}
 instituicoes_id(Id, R) :- solucoes((I, Id), servico(Id, _, I, _), [(R, _)]).
+
+% Extensao do predicado servicos_insituicao: Instituicao, R -> {V,F}
+servicos_insituicao(Instituicao, R) :-
+                                     solucoes((S, Instituicao), servico(_, S, Instituicao, _), L0),
+                                     unicos(L0, L),
+                                     lista_pares_fst(L, R).
+
+% Extensao do predicado servicos_insituicao: Cidade, R -> {V,F}
+servicos_cidade(Cidade, R) :-
+                            solucoes((S, Cidade), servico( _, S, _, Cidade), L0),
+                            unicos(L0, L),
+                            lista_pares_fst(L, R).
+
+% Extensao do predicado servicos_cidade: Data, R -> {V,F}
+servicos_cidade(Cidade, R) :-
+                            solucoes((S, Cidade), servico( _, S, _, Cidade), L0),
+                            unicos(L0, L),
+                            lista_pares_fst(L, R).
