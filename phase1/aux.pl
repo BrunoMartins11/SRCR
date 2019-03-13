dece494213c2571bacbe2b05d56816db591a6655
Filipe Monteiro <pimonteiro@protonmail.com>
2019-03-13 01:00:08 +0000

    almost done

--- phase1/aux.pl
+++ phase1/aux.pl
@@ -21,6 +21,7 @@ lista_pares_fst([(A, _) | T], R) :-
                                   lista_pares_fst(T, L),
                                   append([A], L, R).
 
+<<<<<<< HEAD
 lista_soma([], 0).
 lista_soma([Head | T], R) :-
                           lista_soma(T, R0),
@@ -49,3 +50,10 @@ data(D, 2, A) :-
                A mod 4 =:= 0,
                D >= 1,
                D =< 29.
+=======
+% Extensao do predicado append: L1, L2, L3 -> {V,F}
+append([], List, List).
+append([Head|Tail], List, [Head|Rest]) :-
+append(Tail, List, Rest).
+                                
+>>>>>>> 599f8f6... almost done
